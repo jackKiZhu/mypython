ee89890378000cff374a00563994228321110a73
Maanger <178480849@qq.com>
2018-11-25 16:25:47 +0800

    增加用户视图函数

--- python_test.py
+++ python_test.py
@@ -6,6 +6,11 @@ app = Flask(__name__)
 def index():
     return "my flask web"
 
+@app.route("/user")
+def user_info():
+    return "username:123, userpassword:321"
+
+
 if __name__ == "__main__":
     app.run()
     
